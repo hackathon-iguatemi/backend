12b57c7e2113052593b9a434cb0da0c24d990a71
mcatossi <mcatossi@br.ibm.com>
2018-09-23 04:45:03 -0300

    new version

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require("express"), app = express();
 var port = process.env.PORT || 8080;
 
-
 app.use(express.static(__dirname + '/front/www/'));
 
 var bodyParser = require('body-parser')
